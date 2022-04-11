72ad34784a157d3df897b12d7b790d5cb7d20638
ultimatePandamanqq@gmail.com <ultimatePandamanqq@gmail.com>
2022-04-11 13:51:31 +0200

    Initial

---
+++ diplomka.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers, losses
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Model
+
+(x_train, _), (x_test, _) = fashion_mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+
+print (x_train.shape)
+print (x_test.shape)
